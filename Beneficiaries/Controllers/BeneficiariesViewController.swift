611605d7aba2163b65f05592ee680475822e4869
Erik Perez <erikperez@Eriks-MacBook-Pro.local>
2024-03-12 16:34:11 -0500

    Initial Commit

---
+++ Beneficiaries/Controllers/BeneficiariesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BeneficiariesViewController.swift
+//  Beneficiaries
+//
+//  Created by Erik Perez on 3/12/24.
+//
+
+import UIKit
+
+class BeneficiariesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
